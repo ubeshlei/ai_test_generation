f3d1593837a16d56ce59510f60269321fe94b87d
ubeshlei <ystumys@gmail.com>
2026-03-12 17:43:39 +0200

    update model in chat completion to use gpt-3.5-turbo

--- python_tools/generate_tests.py
+++ python_tools/generate_tests.py
@@ -37,7 +37,7 @@ def generate_test_for_class(file_path):
 
     try:
         response = client.chat.completions.create(
-            model="gpt-4",
+            model="gpt-3.5-turbo",
             messages=messages,
             temperature=0.2,
             max_tokens=1000
