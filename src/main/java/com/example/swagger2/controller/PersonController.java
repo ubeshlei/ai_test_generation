16a683c133a91bd42f906516b45c59e39b8367f7
ubeshlei <ystumys@gmail.com>
2026-03-12 18:55:59 +0200

    modify API documentation for PersonController

--- src/main/java/com/example/swagger2/controller/PersonController.java
+++ src/main/java/com/example/swagger2/controller/PersonController.java
@@ -37,7 +37,7 @@ public class PersonController {
     }
 
     @GetMapping("/{id}")
-    @Operation(summary = "Get a person by Id")
+    @Operation(summary = "Get a person by Idddddd")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200",  description = "User information retrieved successfully"),
             @ApiResponse(responseCode = "400",  description = "Bad request"),
